aa9785d2a3e3ded389029d703e6d02589e219126
anabibia11022009 <anabeatrizdeandradecanga@gmail.com>
2025-06-10 15:16:56 +0000

    aula1

--- scritp.js
+++ scritp.js
@@ -1,7 +1,11 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave'); 
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave); 
+botaoMostraPalavras.addEventListener('click', mostraPalavraSChave); 
 
-function mostraPalavrasChave() { 
-  alert('fui clicado!'); 
+function mostraPalavraSChave() { 
+  const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+    
+    campoResultado.textContent = palavras;
 } 
